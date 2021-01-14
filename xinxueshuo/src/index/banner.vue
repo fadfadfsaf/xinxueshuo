b1f385c41ca2534b8ddaf7838a313770976d10a1
lyx <1092642896@qq.com>
2021-01-14 23:23:11 +0800

    123

--- xinxueshuo/src/index/banner.vue
+++ xinxueshuo/src/index/banner.vue
@@ -1,11 +1,15 @@
 <template>
-  <el-row class="banner">
-    <el-col :span="24" :offset="2" class="lianx">
-      <el-carousel indicator-position="outside">
-        <el-carousel-item v-for="A in 4" :key="A">
-          <h3>{{ A }}</h3>
-        </el-carousel-item>
-      </el-carousel>
+  <el-row >
+    <el-col :span="20" :offset="2" class="banner">
+      <el-row>
+        <el-col :span="15" :offset="2" :gutter="20">
+          <el-carousel indicator-position="outside">
+            <el-carousel-item v-for="A in 4" :key="A">
+              <h3>{{ A }}</h3>
+            </el-carousel-item>
+          </el-carousel>
+        </el-col>
+      </el-row>
     </el-col>
   </el-row>
 </template>
@@ -15,6 +19,11 @@ export default {
 };
 </script>
 <style scoped>
+.banner {
+  margin-top: 60px;
+  background: #fff;
+  box-shadow: 0px 0px 3px #215089;
+}
 .el-carousel__item h3 {
   color: #475669;
   font-size: 18px;
@@ -22,11 +31,9 @@ export default {
   line-height: 300px;
   margin: 0;
 }
-
 .el-carousel__item:nth-child(2n) {
   background-color: #99a9bf;
 }
-
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
 }
