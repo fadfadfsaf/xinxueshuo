35266aa20fb6386d0b4eb73fb6f1ccf40b9b9414
lyx <1092642896@qq.com>
2021-01-14 21:47:35 +0800

    123

--- xinxueshuo/src/component/Footer.vue
+++ xinxueshuo/src/component/Footer.vue
@@ -2,7 +2,7 @@
   <div id="footer">
     <el-row class="footer">
       <el-col :span="4" :offset="2">
-        系我们 Contact Us： 联系：010-52468286 邮箱：info@xinxueshuo.cn
+        联系我们 Contact Us： 联系：010-52468286 邮箱：info@xinxueshuo.cn
         手机：15010927730 地址：北京市朝阳区曙光西里时间国际A座1501 链接 Links：
         微信公众号 教育研究院 新学说文化传媒有限公司 NEWSCHOOL INSIGHT MEDIA
         Copyright © 2018 xinxueshuo.cn All Rights Reserved 京ICP备 16031987号-1
