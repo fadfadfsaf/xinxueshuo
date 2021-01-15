9ad4e9ddd5fdbdda6f079204fa0225e436d2c4f8
lyx <1092642896@qq.com>
2021-01-15 15:22:41 +0800

    we

--- xinxueshuo/src/index/banner.vue
+++ xinxueshuo/src/index/banner.vue
@@ -1,6 +1,15 @@
 <template>
-  <el-row id="banner">
-    <el-col :span="20" :offset="2" class="banner"> sdaas </el-col>
+  <el-row>
+    <el-col :span="20" :offset="2">
+      <div class="block" id="banner">
+        <span class="demonstration">默认 Hover 指示器触发</span>
+        <el-carousel height="450px">
+          <el-carousel-item v-for="item in 4" :key="item">
+            <h3 class="small">{{ item }}</h3>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+    </el-col>
   </el-row>
 </template>
 <script>
@@ -9,11 +18,26 @@ export default {
 };
 </script>
 <style scoped>
-.banner {
+#banner {
   height: 500px;
-  margin: 60px 0px;
+  margin: 80px 0;
   background: #fff;
   box-shadow: 0px 0px 10px #215089;
-  padding: 35px;
+  padding: 25px;
 }
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 450px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+     background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+     background-color: #d3dce6;
+  }
 </style>
